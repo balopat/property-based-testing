1915eeecf095e1bd4299c640a6a751f3a41ac584
Balint Pato <balint.pato@balopat.com>
2016-08-04 20:37:08 -0400

    migrated to new quickcheck lib

--- java/primefactors/src/test/java/primefactors/PrimeFactorsCheck.java
+++ java/primefactors/src/test/java/primefactors/PrimeFactorsCheck.java
@@ -1,50 +1,68 @@
 package primefactors;
 
-import net.java.quickcheck.QuickCheck;
-import org.junit.Before;
-import org.junit.Test;
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Generator;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.runner.RunWith;
 
 import java.io.*;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
-import static java.nio.charset.StandardCharsets.*;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
-import static net.java.quickcheck.generator.PrimitiveGenerators.fixedValues;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
  * Created by balopat on 8/4/16.
  */
+@RunWith(JUnitQuickcheck.class)
 public class PrimeFactorsCheck {
 
-    private List<Integer> primes;
+    public static class Primes extends Generator<Integer> {
 
-    @Before
-    public void readPrimes() throws IOException {
-        InputStream is = getClass().getResourceAsStream("/primes.txt");
-        final Reader r = new InputStreamReader(is, UTF_8);
-        final BufferedReader br = new BufferedReader(r);
+        private List<Integer> primes;
 
-        primes = br.lines()
-                .map(Integer::parseInt)
-                .collect(toList());
-    }
-
-    @Test
-    public void primeFactorsOfPrimesIsThePrimeItself() {
-        for (int i = 0; i < 1000; i++) {
-            Integer n = fixedValues(primes).next();
-            assertThat(PrimeFactors.factorsOf(n),
-                    is(singletonList(n)));
+        public Primes() {
+            super(Integer.class);
+            try {
+                readPrimes();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        protected Primes(Class<Integer> type) {
+            super(type);
 
         }
+
+
+        @Override
+        public Integer generate(SourceOfRandomness sourceOfRandomness, GenerationStatus generationStatus) {
+            return primes.get(sourceOfRandomness.nextInt(0, primes.size() - 1));
+        }
+
+        private void readPrimes() throws IOException {
+            InputStream is = getClass().getResourceAsStream("/primes.txt");
+            final Reader r = new InputStreamReader(is, UTF_8);
+            final BufferedReader br = new BufferedReader(r);
+
+            primes = br.lines()
+                    .map(Integer::parseInt)
+                    .collect(toList());
+        }
+    }
+
+
+    @Property(trials = 100)
+    public void primeFactorsOfPrimesIsThePrimeItself(@From(Primes.class)
+                                                         Integer n) {
+        System.out.println(n);
+        assertThat(PrimeFactors.factorsOf(n),
+                is(singletonList(n)));
     }
 }
